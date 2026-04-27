156da23e8cdf37e43b3b2adbd0c736aa9b21a863
Duncan Sims <duncansims@sfs-wifi-dhcp-10-143-129-106.wifi.sfsu.edu>
2026-04-27 12:15:53 -0700

    redoing pages functions

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
+const PORT = process.env.PORT;
 
 const express = require("express");
 const nunjucks = require("nunjucks");
 const path = require("path");
+
+const pagesRouter = require("./routes/pages");
+const apiRouter = require("./routes/api");
 
 const app = express();
 
@@ -14,11 +18,7 @@ nunjucks.configure("views", {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
-
 app.use(express.static(path.join(__dirname, "public")));
-
-const pagesRouter = require("./routes/pages");
-const apiRouter = require("./routes/api");
 
 app.use("/", pagesRouter);
 app.use("/api", apiRouter);
@@ -27,7 +27,11 @@ app.use((req, res) => {
     res.status(404).send("Page not found");
 });
 
-const PORT = process.env.PORT || 3000;
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).send("Internal server error");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
